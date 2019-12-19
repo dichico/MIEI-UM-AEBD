74e83d7bffe145606a1fffaead2be45fd3601c65
mateussilva98 <mateus_silva_12@hotmail.com>
2019-12-19 17:13:35 +0000

    mudei cores

--- Front-End/Database Manager/js/graficos/sizePieChart.js
+++ Front-End/Database Manager/js/graficos/sizePieChart.js
@@ -4,48 +4,48 @@ Chart.defaults.global.defaultFontColor = '#858796';
 
 
 
-$(document).ready(function(){
-  
+$(document).ready(function () {
+
   var x = Array()
   var y = Array()
 
   var test = 'http://localhost:8080/ords/grupo7/db_status/?q={"$orderby":{"id":"ASC"}}'
   $.getJSON(test, function (json) {
-    
-    item = [json.items.length-1]
-      x.push(json.items[item].free_size)
-      y.push(json.items[item].used_size)
 
-// Pie Chart Example
-var ctx = document.getElementById("sizePieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Livre", "Ocupado"],
-    datasets: [{
-      data: [x,y],
-      backgroundColor: ['#1cc88a', '#e74a3b'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false
-    },
-    cutoutPercentage: 80,
-  },
-})
-})
+    item = [json.items.length - 1]
+    x.push(json.items[item].free_size)
+    y.push(json.items[item].used_size)
+
+    // Pie Chart Example
+    var ctx = document.getElementById("sizePieChart");
+    var myPieChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ["Livre", "Ocupado"],
+        datasets: [{
+          data: [x, y],
+          backgroundColor: ['#1cc88a', '#e74a3b'],
+          hoverBackgroundColor: ['#00802b', '#b30000'],
+          hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+      },
+      options: {
+        maintainAspectRatio: false,
+        tooltips: {
+          backgroundColor: "rgb(255,255,255)",
+          bodyFontColor: "#858796",
+          borderColor: '#dddfeb',
+          borderWidth: 1,
+          xPadding: 15,
+          yPadding: 15,
+          displayColors: false,
+          caretPadding: 10,
+        },
+        legend: {
+          display: false
+        },
+        cutoutPercentage: 80,
+      },
+    })
+  })
 });
