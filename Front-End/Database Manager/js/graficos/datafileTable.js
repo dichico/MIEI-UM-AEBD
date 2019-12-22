c625867dfa2fcc1ca9984019640b5173eacb65f3
Diogo Araújo <36507885+dichico@users.noreply.github.com>
2019-12-22 19:28:29 +0000

    trocas no frontend já

--- Front-End/Database Manager/js/graficos/datafileTable.js
+++ Front-End/Database Manager/js/graficos/datafileTable.js
@@ -1,5 +1,5 @@
 // Function para adicionar as linhas da tabela
-function appendToDataFileTable (ID_DATAFILE, NAME, AUTO_EXTENSIBLE, STATUS, MAX_BYTES, BYTES, NAME_TABLESPACE) {
+function appendToDataFileTable (ID_DATAFILE, NAME, AUTO_EXTENSIBLE, STATUS, NAME_TABLESPACE) {
   
     var table = document.getElementById('datafileTable');
     var newRow = document.createElement('tr');
@@ -8,8 +8,6 @@ function appendToDataFileTable (ID_DATAFILE, NAME, AUTO_EXTENSIBLE, STATUS, MAX_
       <th>${NAME}</th>
       <th>${AUTO_EXTENSIBLE}</th>
       <th>${STATUS}</th>
-      <th>${MAX_BYTES}</th>
-      <th>${BYTES}</th>
       <th>${NAME_TABLESPACE}</th>
     `;
 
@@ -17,13 +15,13 @@ function appendToDataFileTable (ID_DATAFILE, NAME, AUTO_EXTENSIBLE, STATUS, MAX_
   }
   
 $(document).ready(function() {
-    var data = 'http://localhost:8080/ords/grupo7/personalizado/1'
+    var data = 'http://localhost:8080/ords/grupo7/personalizado/1?limit=500'
     $.getJSON(data, function (json) {
           
       // Ciclo for para cada item -> linha
       for (var item of json.items) {
         var ts = new Date(item.timestamp)
-        appendToDataFileTable(item.id, item.name, item.auto_extensible, item.status, item.max_bytes, item.bytes, item.tablespace_name)
+        appendToDataFileTable(item.id, item.name, item.auto_extensible, item.status, item.tablespace_name)
       }
     
     })
